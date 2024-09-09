cb3097cb00724daa710ba8b1ee064d212b4bab47
Sarah Yack <sarah@yack.email>
2024-09-09 17:04:38 -0600

    Finished Bee Movement

--- Scenes/bee.gd
+++ Scenes/bee.gd
@@ -1,20 +1,66 @@
 extends Area2D
 
+@export var marker1: Marker2D
+@export var marker2: Marker2D
+@export var speed := 25
+@export var speed_boost := 10
 @export var health := 3
 @export var attack_strength := 1
 
+@onready var target = marker2
+@onready var player = get_tree().get_first_node_in_group("player")
+
+var forward := true
+var normal := true
+var aggressive := false
+
 # Main Functions
 
-func _process(_delta):
+func _ready():
+	position = marker1.position
+
+func _process(delta):
 	check_death()
+	get_target()
+	flip_logic()
+	
+	position += (target.position - position).normalized() * speed * delta
+	
 
 # Movement Functions
+
+func get_target():
+	if forward and position.distance_to(marker2.position) < 10 or\
+		not forward and position.distance_to(marker1.position) < 10:
+		forward = not forward
+	
+	if aggressive:
+		target = player
+	elif forward:
+		target = marker2
+	else:
+		target = marker1
 
 # Status Functions
 
 func check_death():
 	if health <= 0:
 		queue_free()
+
+func flip_logic():
+	$Sprite2D.flip_h = not forward
+	if aggressive:
+		$Sprite2D.flip_h = position.x > player.position.x
+
+func go_aggressive():
+	aggressive = true
+	if aggressive:
+		speed += speed_boost
+
+func reset_aggressive():
+	aggressive = false
+	if normal:
+		speed -= speed_boost
 
 # Signal Functions
 
@@ -28,6 +74,12 @@ func _on_area_entered(area):
 func _on_body_entered(body):
 	if body.is_in_group("player"):
 		body.damage(attack_strength)
+
+func _on_detection_range_body_entered(_body):
+	go_aggressive()
+
+func _on_detection_range_body_exited(_body):
+	reset_aggressive()
 
 
 # Timeout Functions
